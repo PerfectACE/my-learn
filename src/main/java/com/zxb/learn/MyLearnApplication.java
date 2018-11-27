28854e669e245b7e6ed7212303923f5073ec4325
左晓彬 <perfectace@163.com>
2018-11-27 16:35:28 +0800

    init

---
+++ src/main/java/com/zxb/learn/MyLearnApplication.java
@@ -0,0 +1,12 @@
+package com.zxb.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLearnApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zxb/learn/MyLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxb.learn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyLearnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
